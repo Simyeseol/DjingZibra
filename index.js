b9c938b44670e3cd973bc1416a8f8d21b938d184
Simyeseol <43331033+Simyeseol@users.noreply.github.com>
2020-07-19 21:46:28 +0900

    ~express server
    -

---
+++ index.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const app = express();
+
+const PORT = 4000;
+
+function handleListening() {
+    console.log(`Listening on: http://localhost:4000${PORT}`);
+}
+app.listen(PORT, handleListening);
